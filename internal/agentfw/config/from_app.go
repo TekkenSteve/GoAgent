6690e410b7a39a01192f3b6ef675353723b87bff
TekkenSteve <183297386@qq.com>
2026-06-14 20:07:42 +0800

    refactor: internalize application config

--- internal/agentfw/config/from_app.go
+++ internal/agentfw/config/from_app.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"time"
 
-	appconfig "github.com/TekkenSteve/GoAgent/config"
+	appconfig "github.com/TekkenSteve/GoAgent/internal/config"
 )
 
 // FromAppConfig maps application config into the framework-specific config model.
